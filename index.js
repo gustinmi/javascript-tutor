5caf3186c5e9d4975a3cef8682a3ac8483f6ae9b
MitjaGustin <gustinmi@gmail.com>
2019-04-16 13:53:05 +0200

    comments

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-var http = require('http');
-var fs = require('fs');
+// Simple nodejs server
 
-var html = fs.readFileSync('index.html');
+const http = require('http');
+const fs = require('fs');
 
-http.createServer(function (req, res) {
+// read local file synchronous
+const html = fs.readFileSync('index.html');
 
+// create server that listens to port
+http.createServer(function (req, res) { // when client connects, execute this callback
+
+	// write headers
 	res.writeHead(200, {'Content-Type': 'text/html'});
+    
+    // write file contents
     res.end(html);
 
 }).listen(8000);
