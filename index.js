93a509940d12f3f39549b183f00521d8a4026430
Mitja Guštin <gustinmi@gmail.com>
2019-04-17 17:38:19 +0200

    popravljen file

--- index.js
+++ index.js
@@ -1,18 +1,50 @@
 // Simple nodejs server
+"use strict";
 
 const http = require('http');
 const fs = require('fs');
 
-// read local file synchronous
-const html = fs.readFileSync('index.html');
-
 // create server that listens to port
-http.createServer(function (req, res) { // when client connects, execute this callback
+http.createServer(function(req, response) { // when client connects, execute this callback
 
-	// write headers
-	res.writeHead(200, {'Content-Type': 'text/html'});
+	let vsebina;
+
+    if (req.url == "/") { // vse kar ne vemo kaj je
+    	debugger;
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+
+    } else if (req.url == "/index.html") { // index.html
+    	debugger;
+
+        // read local file synchronous
+        vsebina = fs.readFileSync('index.html');
+
+        // write headers
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+
+    } else if (req.url == "/mesta") {
+    	debugger;
+
+        // write headers
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+
+        // TODO povezi se na mysql
+
+
+    } else {
+    	debugger;
+
+    	console.log("Napaka, ne razumem zahteve. ");
+    	response.writeHead(500);
+
+    }
+
+    if (vsebina){
+    	// write file contents
+    	response.end(vsebina);
+    }else{
+    	response.end();
+    }
     
-    // write file contents
-    res.end(html);
 
 }).listen(8000);
